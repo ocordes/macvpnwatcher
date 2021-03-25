e8e0faaa9a4c215f8492e55d63a6f765d6eb0c77
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-03-25 16:49:18 +0100

    - do some tests with VPN detection, try a simple menu structure

---
+++ src/vpn.py
@@ -0,0 +1,43 @@
+"""
+src/vpn.py
+
+written by: Oliver Cordes 2021-03-24
+changed by: Oliver Cordes 2021-03-25
+"""
+
+import re
+import subprocess
+
+def execute_command(cmd):
+    p = subprocess.Popen(cmd, shell=True, bufsize=-1, close_fds=True,
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.STDOUT)
+
+    # read all lines and convert to utf-8 and split into lines
+    return p.stdout.read().decode('utf-8').split('\n')
+
+
+    
+
+def list_of_vpn_connections():
+    conns = {}
+    result = execute_command('scutil --nc list')
+    rex = re.compile(r'.*[(](?P<status>[a-zA-Z]+)[)].*[\"](?P<name>[a-zA-Z0-9_-]+)[\"].*(\[)(?P<type>[a-zA-Z0-9]+)(\])')
+    for line in result:
+        if line != '' and line[0] == '*':  # enabled connection
+            m = rex.search(line)
+            if m is not None:
+                conns[m.group('name')] = m.groupdict()
+
+    return conns
+
+
+def status_vpn_connection(name):
+    result = execute_command(f'scutil --nc status {name}')
+
+
+
+if __name__ == '__main__':
+    print(list_of_vpn_connections())
+    print('Done.')
--- src/watcher.py
+++ src/watcher.py
@@ -2,22 +2,69 @@
 src/watcher.py
 
 written by: Oliver Cordes 2021-03-23
-changed by: Oliver Cordes 2021-03-23
+changed by: Oliver Cordes 2021-03-25
 """
 
 import rumps
 
 from helper import isDarkMode
 
+from vpn import list_of_vpn_connections
+
+rumps.debug_mode(True)
+
+# mimik a callback_template to feed the connection name 
+# as a parameter
+def callback_template(name, func):
+    def callback(sender):
+        func(name, sender)
+
+    return callback
+
+
 class MacVPNWatcherApp(object):
     def __init__(self):
         #self.app = rumps.App('MacVPNWatcher', '🍅')
 
         if isDarkMode:
-            icon='../icons/appicon-dark.icns'
+            icon='icons/appicon-dark.icns'
         else:
-            icon='../icons/appicon.icns'
+            icon='icons/appicon.icns'
         self.app = rumps.App('MacVPNWatcher', icon=icon)
+
+        self._connections = {}
+        self._menu_items = {}
+        self.update_menu()
+
+
+    def update_menu(self):
+        conns = list_of_vpn_connections()
+        # add connections from VPN list
+        for conn in sorted(conns.keys()):
+            if conn not in self._connections:
+                menuitem = rumps.MenuItem(conn, callback=callback_template(conn, self.clicked_connection))
+                #menuitem = rumps.MenuItem(conn, callback=self.click)
+                self._menu_items[conn] = menuitem
+                self._connections[conn] = conns[conn]
+
+        # look for removed connections
+        for conn in self._connections:
+            if conn not in conns:
+                self._connections.pop(conn)
+
+        print(self._connections)
+
+        self.app.menu = [self._menu_items[i] for i in self._menu_items]
+
+
+    def click(self, sender):
+        print('click', sender)
+
+
+
+    def clicked_connection(self, name, sender):
+        print('clicked:', name)
+
 
     def run(self):
         self.app.run()
